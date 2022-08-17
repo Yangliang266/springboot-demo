dd1524ebdc9069aad6926b355b03f73662a5c632
xiaomi <546493589@qq.com>
2022-08-17 14:27:31 +0800

    testfinal1

--- src/main/java/com/gupao/springbootdemo/controller/K8SController.java
+++ src/main/java/com/gupao/springbootdemo/controller/K8SController.java
@@ -11,6 +11,6 @@ public class K8SController {
 
     @RequestMapping("/k8s")
     public String k8s(){
-        return "it is ok real, i am man , do you like and you";
+        return "it is ok real, i am man , do you like and you, fighting";
     }
 }
